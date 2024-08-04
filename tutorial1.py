d633c6431b29c28cf91ee91a82bf0f622f175158
Vaibhav Gupta <vaibhav101098@gmail.com>
2024-08-04 16:06:07 +0530

    first day completed

--- tutorial1.py
+++ tutorial1.py
@@ -13,13 +13,22 @@ person = Person("Mike", 30, 180) """
 
 # if we want to delete whole Person object then just define del function
 class Person:
+    amount = 0
     def __init__(self, name, age, height):
         self.name = name
         self.age = age
-        self.height = height 
-
+        self.height = height
+        Person.amount += 1
+    
     def __del__(self):
-        print("Person object deleted")
+        Person.amount -= 1
+
+    """ def __del__(self):
+        print("Person object deleted") """
 
 person = Person("Mike", 30, 180)
-del Person
+
+person2 = Person("Sarah", 28, 175)
+
+# del person2
+print(Person.amount)
