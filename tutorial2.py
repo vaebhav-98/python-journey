ceb1e0d24bf9fe71f4849c8f25b1f8a39ce7f6f9
Vaibhav Gupta <vaibhav101098@gmail.com>
2024-08-07 00:19:35 +0530

    vector addition and subtration using operator overloading

--- tutorial2.py
+++ tutorial2.py
@@ -28,5 +28,30 @@ class Worker(Person):
         return self.salary * 12
 
 worker1 = Worker("Henry", 40, 176, 300)
-print(worker1)
-print(worker1.calc_yearly_salary())
+""" print(worker1)
+print(worker1.calc_yearly_salary()) """
+
+
+# Operator Overloading
+
+class Vector:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def __add__(self, other):
+        return Vector(self.x + other.x, self.y + other.y)
+    
+    def __sub__(self, other):
+        return Vector(self.x - other.x, self.y - other.y)
+    
+    def __str__(self):
+        return f"X: {self.x}, Y: {self.y}"
+
+vector1 = Vector(2, 5)
+vector2 = Vector(6, 3)
+
+print(vector1)
+print(vector2)
+
+print(vector1 - vector2)
