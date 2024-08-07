684c37839d2057f62964405e72aff7c89116eebe
Vaibhav Gupta <vaibhav101098@gmail.com>
2024-08-08 00:06:07 +0530

    problem 1

---
+++ problem1.py
@@ -0,0 +1,30 @@
+# Hackerrank solution
+""" 
+Consider a list (list = []). You can perform the following commands:
+
+insert i, e: Insert integer e at position i.
+print: Print the list.
+remove e: Delete the first occurrence of integer e.
+append e: Insert integer e at the end of the list.
+sort: Sort the list.
+pop: Pop the last element from the list.
+reverse: Reverse the list.
+Initialize your list and read in the value of n followed by n lines of commands where each command will be of the 7 types listed above. 
+Iterate through each command in order and perform the corresponding operation on your list. """
+
+""" Example
+N = 4
+
+append 1
+
+append 2
+
+insert 3 1
+
+print
+
+append 1: Append 1 to the list, arr = [1].
+append 2: Append 2 to the list, arr = [1, 2].
+insert 3 1: Insert 3 at index 1,arr = [1, 3, 2].
+print: Print the array. """
+
